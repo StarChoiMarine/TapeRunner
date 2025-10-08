05e7bf113679367eccf77e2a4e11e2fdfc150a74
StarChoiMarine <sung000054@gmail.com>
2025-10-08 23:40:39 +0900

    앱 수정

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen.tsx';
-import SignupScreen from './src/screens/SignupScreen.tsx';
-import HomeScreen from './src/screens/HomeScreen.tsx';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import LoginScreen from './src/screens/LoginScreen';   // ← 확장자 빼는 걸 권장
+import SignupScreen from './src/screens/SignupScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 export type RootStackParamList = {
   Login: undefined;
@@ -15,15 +17,18 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerTitleAlign: 'center' }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title: '로그인' }} />
-        <Stack.Screen name="Signup" component={SignupScreen} options={{ title: '회원가입' }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Tape Runner' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerTitleAlign: 'center' }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} options={{ title: '로그인' }} />
+          <Stack.Screen name="Signup" component={SignupScreen} options={{ title: '회원가입' }} />
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Tape Runner' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
+
